00d2b70f2707f61422a979915c94909327aefa5c
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2022-03-17 12:59:40 +0100

    update button more

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,13 +1,20 @@
-(() => {
-  function toggleReadMore(e) {
-    const dots = e.target.previousElementSibling.querySelector('.products__text--dots');
-    const moreText = e.target.previousElementSibling.querySelector('.products__text--more');
+// import * as arrowsImgs from '../images/btn-arrow/*.png';
 
-    dots.classList.toggle('hidden');
-    moreText.classList.toggle('hidden');
-  }
+const productsBtnMore = document.querySelectorAll('.products__button');
 
-  const productBtns = document.getElementsByClassName('products__button');
+productsBtnMore.forEach(btn => btn.addEventListener('click', toggleReadMore));
 
-  Array.from(productBtns).forEach(btn => btn.addEventListener('click', toggleReadMore));
-})();
+function toggleReadMore(e) {
+  const dots = e.target.previousElementSibling.querySelector('.products__text--dots');
+  const moreText = e.target.previousElementSibling.querySelector('.products__text--more');
+
+  dots.classList.toggle('hidden');
+  moreText.classList.toggle('hidden');
+  e.target.classList.toggle('top');
+
+  // if (!dots.classList.contains('hidden')) {
+  //   e.target.style.backgroundImage = `url(${arrowsImgs['arrow-right@2x']})`;
+  // } else {
+  //   e.target.style.backgroundImage = `url(${arrowsImgs['arrow-top@2x']})`;
+  // }
+}
